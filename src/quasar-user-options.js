ddacf41d01da39ddab627e530a41124fec218bdc
Mohamed Khaled <90722444+kaskth@users.noreply.github.com>
2023-05-12 00:30:14 +0300

    Delete src directory

--- src/quasar-user-options.js
+++
@@ -1,9 +0,0 @@
-import './styles/quasar.scss'
-import '@quasar/extras/roboto-font/roboto-font.css'
-import '@quasar/extras/material-icons/material-icons.css'
-import {Loading,Notify} from 'quasar'
-// To be used on app.use(Quasar, { ... })
-export default {
-  config: {loading:{},Notify:{}},
-  plugins: {Loading,Notify}
-}
